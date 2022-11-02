7a36cf81764b616a09ce1e91ecdb4370d273c9c5
strawbrea <110314654+strawbrea@users.noreply.github.com>
2022-11-02 13:37:47 -0400

    Create q6.m
    newton's method, question 6 for homework 5/6

---
+++ methods/HW 5_6/q6.m
@@ -0,0 +1,18 @@
+function x = q6(f, df, a, tol, nmax)
+x = a;
+con = 0; %0 means not converged, 1 means converged
+
+for i=0:1:nmax
+    prev = x;
+    x = x - f(x)/df(x);
+    disp(i + ": " + x)
+    if abs(f(x)) + abs(x-prev) < tol
+        fprintf('converge');
+        con = 1;
+        break;
+    end
+end
+if i == nmax && con == 0
+    fprintf('did not converge');
+end
+end
