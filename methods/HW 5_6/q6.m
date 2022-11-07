ce8e3be5d645852f9dd15c1500512497ebfd180d
breanna <110314654+strawbrea@users.noreply.github.com>
2022-11-06 23:55:52 -0500

    Update q6.m

--- methods/HW 5_6/q6.m
+++ methods/HW 5_6/q6.m
@@ -1,8 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%    Breanna Lowery    %%
+%%     Homework 5_6     %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function x = q6(f, df, a, tol, nmax)
 x = a;
 con = 0; %0 means not converged, 1 means converged
 
-for i=0:1:nmax
+for i=1:1:nmax
     prev = x;
     x = x - f(x)/df(x);
     disp(i + ": " + x)
